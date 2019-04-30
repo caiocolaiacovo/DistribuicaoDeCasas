536b4f2a9dab2b29643072dbcfc6df591fafce26
Caio Colaiacovo Carneiro da Costa <caiohshot@gmail.com>
2019-04-30 01:44:31 -0400

    Criando camadas de Aplicação e infra

---
+++ src/DistribuicaoDeCasas.Aplicacao/Contratos/IContemplados.cs
@@ -0,0 +1,9 @@
+using DistribuicaoDeCasas.Dominio.Contratos;
+
+namespace DistribuicaoDeCasas.Aplicacao.Contratos
+{
+    public interface IContemplados
+    {
+         void ContemplarFamilia(IFamilia familia);
+    }
+}
---
+++ src/DistribuicaoDeCasas.Infra/Contemplados.cs
@@ -0,0 +1,13 @@
+using DistribuicaoDeCasas.Aplicacao.Contratos;
+using DistribuicaoDeCasas.Dominio.Contratos;
+
+namespace DistribuicaoDeCasas.Infra
+{
+    public class Contemplados : IContemplados
+    {
+        public void ContemplarFamilia(IFamilia familia)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
